e07472a865b800e9e5bdb14a1e7193809c5db9a6
lengkimtry <kimt47494@gmail.com>
2025-02-16 20:12:37 +0700

    Refactor layout structure in Page component for improved responsiveness

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,34 +1,34 @@
-import ActionButton from '@/components/ActionButton';
-import Drawer from '@/components/Drawer';
-import React from 'react';
-import Welcome from './Welcome';
-import Header from '@/components/Header';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import { AppSidebar } from '@/components/AppSideBar';
+import ActionButton from "@/components/ActionButton";
+import Drawer from "@/components/Drawer";
+import React from "react";
+import Welcome from "./Welcome";
+import Header from "@/components/Header";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSideBar";
 
 function Page() {
   return (
-    <div className="flex flex-col h-screen">
-      <Header/>
-
-      <SidebarProvider>
-        
-        <div className="flex flex-grow">
-          
-          
-          <AppSidebar />
-          <SidebarTrigger />
-
-          <div className="flex flex-grow items-center justify-center bg-gray-200 text-center">
-            <div className="flex flex-col items-center gap-4">
-              
-              <Welcome username={'Someth'} />
-              <ActionButton />
+    <SidebarProvider>
+      <div className="flex flex-col w-screen h-screen">
+        <Header className="w-full" />
+        <div className="flex flex-grow bg-gray-200">
+          <div className="flex w-full ">
+            <div className="flex w-full ">
+              <AppSidebar />
+              <div className="flex flex-grow">
+                <SidebarTrigger />
+                <div className="flex flex-grow items-center justify-center bg-gray-200 text-center">
+                  <div className="flex flex-col items-center gap-4">
+                    <Welcome username={"Someth"} />
+                    <ActionButton />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </SidebarProvider>
-    </div>
+      </div>
+    </SidebarProvider>
   );
 }
 
