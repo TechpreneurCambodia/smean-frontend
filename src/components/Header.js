ea01f4916033b6ed4e4785cfb7a1bb12d43021d5
lengkimtry <kimt47494@gmail.com>
2025-02-17 14:11:16 +0700

    Adjust Header component's margin for improved responsiveness on different screen sizes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ function Header() {
   const { isHeaderMoved } = useSidebar();
   return (
     <header
-      className={`flex items-center p-4 bg-white shadow-md transition-transform duration-300 ${
-        isHeaderMoved ? "ml-0" : "ml-[16rem]"
+      className={`flex items-center p-4 bg-white shadow-md transition-transform duration-500 ${
+        isHeaderMoved ? "ml-0" : "ml-0 md:ml-[16rem]"
       }`}
     >
       <FaBook className="text-xl text-black" />
