52e57aa1f2f227f1ef27e4799fe39867589661ea
SophaltheanySean <sophaltheanysean@gmail.com>
2025-02-22 18:28:28 +0700

    Completed UploadTranscript & Summary & RecordingPage

---
+++ src/app/recordingpage/page.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import SummaryNoDetail from '../../components/SummaryNoDetail'
+import TimeAndRecorder from '../../components/TimeAndRecorder'
+import Logo from '../../components/Logo'
+import MinuteOption from '../../components/MinuteOption'
+import DateTime from '../../components/DateTime'
+
+
+
+function page() {
+  return (
+    <div className='m-4'>
+        <Logo/>
+        <DateTime/>
+        <div className='flex justify-start'>
+        <h2 className="font-bold text-[32px] flex items-center gap-2 ">
+      កំណត់ពេលវេលាថត៖
+        </h2>
+        <div className='ml-4'>
+        <MinuteOption/>
+        </div>
+        
+        </div>
+     
+        
+              
+        <SummaryNoDetail/>
+        <TimeAndRecorder/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/MinuteOption.js
@@ -0,0 +1,30 @@
+"use client"
+import React, { useState } from 'react';
+
+function MinuteOption() {
+  
+  const [selectedChoice, setSelectedChoice] = useState('open or close');
+
+ 
+  const handleSelection = (choice) => {
+    setSelectedChoice(choice); 
+  };
+
+  return (
+    <div>
+      <details className="dropdown">
+        <summary className="btn m-1">{selectedChoice}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+          <li>
+            <a onClick={() => handleSelection('5 នាទី')}>5 នាទី</a>
+          </li>
+          <li>
+            <a onClick={() => handleSelection('20 នាទី')}>20 នាទី</a>
+          </li>
+        </ul>
+      </details>
+    </div>
+  );
+}
+
+export default MinuteOption;
