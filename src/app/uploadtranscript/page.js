24b38f85da4f7984870ed1f3d62a70a44540b33c
SsethiSak <sethisaksan@gmail.com>
2025-02-22 09:05:23 +0700

    fixed path issue

--- src/app/uploadtranscript/page.js
+++ src/app/uploadtranscript/page.js
@@ -1,40 +1,26 @@
+import React from "react";
 
-import React from 'react';
-
-import SummaryTranscript from '../../components/SummaryTranscript';
-import TimeAndRecorder from '../../components/TimeAndRecorder';
-import Logo from '../../components/Logo';
-
-
+import SummaryTranscript from "../../components/SummaryTranscript";
+import TimeAndRecorder from "../../components/TimeAndRecorder";
+import Logo from "../../components/Logo";
 
 function page() {
   return (
     <div>
-      
-      <Logo/>
+      <Logo />
       <h2 className="font-bold text-[32px] flex items-center gap-2">
-          📖 ប្រតិចារិក/Transcript
-        </h2>
-              <h2 className="font-regular text-[20px] flex items-center gap-2">
-          AMK-meeting.wav
-        </h2>
-        
-<SummaryTranscript/>
-<div className=' mb-16 '>
-<TimeAndRecorder/>
-</div>
+        📖 ប្រតិចារិក/Transcript
+      </h2>
+      <h2 className="font-regular text-[20px] flex items-center gap-2">
+        AMK-meeting.wav
+      </h2>
 
-
-
-
-
-      
-        
-  
+      <SummaryTranscript />
+      <div className=" mb-16 ">
+        <TimeAndRecorder />
+      </div>
     </div>
-    
-    
-  )
+  );
 }
 
 export default page;
