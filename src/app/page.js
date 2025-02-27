3ab35fc56ad39c591916c5062e1eecb0cc61ad55
PhaySometh <phay.someth70@gmail.com>
2025-02-28 00:19:42 +0700

    Configure Sidebar and Header + adding it as layout to every page

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,30 @@
-"use client";
-import React from "react";
-import { useRouter } from "next/navigation";
-import InputField from "@/components/InputField";
-import Button from "@/components/Button";
-import SocialLogin from "@/components/SocialLogin";
-import Link from "next/link";
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Layout from '@/components/Layout';
+import InputField from '@/components/InputField';
+import Button from '@/components/Button';
+import SocialLogin from '@/components/SocialLogin';
+import Link from 'next/link';
 
 export default function Page() {
-  const router = useRouter(); // Initialize the router
+    const router = useRouter(); // Initialize the router
 
-  const handleLogin = () => {
-    // Perform authentication logic here if needed
-    router.push("/home"); // Redirect to the dashboard or another page
-  };
+    const handleLogin = () => {
+        // Perform authentication logic here if needed
+        router.push('/home'); // Redirect to the dashboard or another page
+    };
 
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-        <p>Landing page</p>
-        <a href="/login">Login</a><br></br>
-        <a href="/signup">Signup</a>
-      </div>
-    </div>
-  );
+    return (
+        <Layout>
+            <div className="flex items-center justify-center h-screen bg-gray-200">
+                <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+                    <p>Landing page</p>
+                    <a href="/login">Login</a>
+                    <br></br>
+                    <a href="/signup">Signup</a>
+                </div>
+            </div>
+        </Layout>
+    );
 }
