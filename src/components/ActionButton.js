9dc6677e04ea1d4b678b48196f4eebc809b6b4eb
SsethiSak <sethisaksan@gmail.com>
2025-03-06 01:33:32 +0700

    fixed routing navigation

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -7,10 +7,10 @@ function ActionButton() {
   const router = useRouter();
   return (
     <div className="flex justify-center gap-4">
-    <button className="flex items-center px-6 py-3 bg-white text-primary rounded-lg shadow">
+    <button className="flex items-center px-6 py-3 bg-white text-primary rounded-lg shadow" onClick={() => router.push('/recordingpage')}>
       <FaBook className="mr-2" /> ថតសំឡេងភ្លាមៗ
     </button>
-    <button className="flex items-center px-6 py-3 bg-white text-primary rounded-lg shadow" onClick={() => router.push('/uploadAudio')}>
+    <button className="flex items-center px-6 py-3 bg-white text-primary rounded-lg shadow" onClick={() => router.push('/upload')}>
       <FaUpload className="mr-2" /> បញ្ចូលប្រតិចារិក/Upload
     </button>
   </div>
