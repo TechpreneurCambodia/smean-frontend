1de1eafca3e34c4f94435c0eec4382fc665b1af1
PhaySometh <phay.someth70@gmail.com>
2025-03-13 23:51:15 +0700

    fixed the minute option dropdown

--- src/components/MinuteOption.js
+++ src/components/MinuteOption.js
@@ -1,24 +1,30 @@
 "use client"
 import React, { useState } from 'react';
-import {ChevronDown, ChevronUp} from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 function MinuteOption() {
-  
   const [selectedChoice, setSelectedChoice] = useState('កំណត់ពេលវេលាថត');
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleSelection = (choice) => {
-    setSelectedChoice(choice); 
+    setSelectedChoice(choice);
+    setIsOpen(false);
   };
 
   return (
     <div>
-      <details className="dropdown">
-        <ChevronDown />
-        <ChevronUp />
-        <summary className="btn m-1">{selectedChoice}</summary>
-        <ul className="text-black menu dropdown-content border-2 border-black bg-primary70 rounded-box z-[1] w-52 p-2 shadow">
+      <details 
+        className="dropdown" 
+        open={isOpen}
+        onToggle={(e) => setIsOpen(e.target.open)}
+      >
+        <summary className="btn border-none bg-primary texz m-1 flex items-center justify-between">
+          <span>{selectedChoice}</span>
+          {isOpen ? <ChevronUp /> : <ChevronDown />}
+        </summary>
+        <ul className="text-white font-bold menu dropdown-content bg-primary rounded-box z-[1] w-48 p-2 shadow ml-1">
           <li>
-            <a  onClick={() => handleSelection('3 នាទី')}>3 នាទី</a>
+            <a onClick={() => handleSelection('3 នាទី')}>3 នាទី</a>
           </li>
           <li>
             <a onClick={() => handleSelection('5 នាទី')}>5 នាទី</a>
