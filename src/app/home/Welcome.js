8de56d53e0d56ddb55558ad49e94075f7839b033
SsethiSak <sethisaksan@gmail.com>
2025-03-16 17:39:07 +0700

    highlight color of username and smean

--- src/app/home/Welcome.js
+++ src/app/home/Welcome.js
@@ -1,10 +1,11 @@
-
-function Welcome({username}) {
+function Welcome({ username }) {
   return (
-    <div className="text-[#64748B] text-[40px] leading-[normal] font-[600]" >
-    សួស្តី {username}!​ សូមស្វាគមន៍មកកាន់ ស្មៀន
-  </div>
-  )
+    <div className="text-[#64748B] text-[40px] leading-[normal] font-[600]">
+      សួស្តី <span className="text-[#34d399] font-bold">{username}</span>!
+      សូមស្វាគមន៍មកកាន់
+      <span className="text-primary font-bold"> ស្មៀន</span>
+    </div>
+  );
 }
 
-export default Welcome
+export default Welcome;
