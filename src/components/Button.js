ae39b931bf2d7cc4638df0b4c1956d7dcbbe8d08
SsethiSak <sethisaksan@gmail.com>
2025-03-17 10:48:46 +0700

    button refactor

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,23 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Button = ({ text, onClick }) => {
   return (
-    <button className="btn btn-primary w-full bg-primary text-white border-none" onClick={onClick}>
+    // <button className="btn btn-primary w-full bg-primary text-white border-none" onClick={onClick}>
+    //   {text}
+    // </button>
+
+    /* From Uiverse.io by ItsEnigma143 */
+    <motion.div
+    className="w-full bg-transparent flex items-center justify-center border-2 border-primary shadow-lg text-primary cursor-pointer duration-300 rounded-md hover:bg-primary hover:text-white active:scale-[0.98]"
+    whileHover={{ scale: 1.05 }}
+    whileTap={{ scale: 0.95 }}
+  >
+    <button className="px-5 py-2" onClick={onClick}>
       {text}
     </button>
+  </motion.div>
   );
 };
 
